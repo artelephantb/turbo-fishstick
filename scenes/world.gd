32689fe725d70d93f65abebd82b951d5915a01ea
ArtElephantB <87944666+artelephantb@users.noreply.github.com>
2025-08-26 20:24:45 -0400

    Made better structure system

--- scenes/world.gd
+++ scenes/world.gd
@@ -3,17 +3,16 @@ extends Node2D
 @export var tile_layer: TileMapLayer
 @export var seed = 100
 
-@export_category('Structure')
-@export var srtructure: Array
-@export var srtructure_scale: Vector2i = Vector2i(4, 4)
+func place_structure(structure: Array, position: Vector2i):
+	for height in range(len(structure)):
+		for length in range(len(structure[height])):
+			if structure[height][length] == 0:
+				tile_layer.erase_cell(Vector2i(length, height) + position)
+			else:
+				tile_layer.set_cell(Vector2i(length, height), structure[height][length] - 1, Vector2i(0, 0))
 
 func _ready() -> void:
-	for height in range(srtructure_scale.y):
-		for length in range(srtructure_scale.x):
-			if length + height * srtructure_scale.x >= len(srtructure) or srtructure[length + height * srtructure_scale.x] == -1:
-				tile_layer.erase_cell(Vector2i(length, height))
-			else:
-				tile_layer.set_cell(Vector2i(length, height), srtructure[length + height * srtructure_scale.x], Vector2i(0, 0))
+	place_structure([[0, 1, 0], [2, 3, 2], [0, 1, 0]], Vector2i(0, 0))
 
 func _process(delta: float) -> void:
 	pass
