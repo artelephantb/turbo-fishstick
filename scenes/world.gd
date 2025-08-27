68ddb1fc85cf05bc1c98a4ca5ced29ca971d45a0
ArtElephantB <87944666+artelephantb@users.noreply.github.com>
2025-08-26 21:09:38 -0400

    More random tiles

--- scenes/world.gd
+++ scenes/world.gd
@@ -15,13 +15,16 @@ func place_structure(structure: Array, location: Vector2i):
 			else:
 				tile_layer.set_cell(Vector2i(length, height) + location, structure[height][length] - 1, Vector2i(0, 0))
 
-func _ready() -> void:
+func place_chunk(location: Vector2i):
 	for chunk_x in range(15):
 		for chunk_y in range(15):
 			if chunk_y <= 0:
-				place_structure(STRUCTURE_TOPS[(set_seed * chunk_x + chunk_y) % len(STRUCTURE_TOPS)], Vector2i(chunk_x * 3, chunk_y * 3))
+				place_structure(STRUCTURE_TOPS[(set_seed * (chunk_x + location.x + chunk_y + location.y)) % len(STRUCTURE_TOPS)], Vector2i(chunk_x * 3, chunk_y * 3) + location)
 			if chunk_y > 0:
-				place_structure(STRUCTURE_GROUNDS[(set_seed * chunk_x + chunk_y) % len(STRUCTURE_GROUNDS)], Vector2i(chunk_x * 3, chunk_y * 3))
+				place_structure(STRUCTURE_GROUNDS[(set_seed * (chunk_x + location.x + chunk_y + location.y)) % len(STRUCTURE_GROUNDS)], Vector2i(chunk_x * 3, chunk_y * 3) + location)
+
+func _ready() -> void:
+	place_chunk(Vector2i(1, 0))
 
 func _process(delta: float) -> void:
 	pass
